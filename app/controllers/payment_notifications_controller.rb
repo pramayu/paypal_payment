aacfd55df1546c0424dce6464e31db8665921175
pramayu <gedepramayu@gmail.com>
2016-09-16 13:59:36 +0700

    Payment Notify

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,8 +1,8 @@
 class PaymentNotificationsController < ApplicationController
   
-  protect_from_forgery except: [:index]
+  protect_from_forgery except: [:create]
   
-  def index
+  def create
     PaymentNotification.create!(:params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
     render :nothing => true
   end
